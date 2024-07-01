484c66bbe338e6bffb5c2899ebcdcebef17994d3
heesunyooon <hee_place@naver.com>
2024-07-02 00:07:22 +0900

    init

---
+++ src/main/java/com/ideacraft/common/ApplicationStarter.java
@@ -0,0 +1,15 @@
+package com.ideacraft.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ideacraft.*")
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+
+}
---
+++ src/main/java/com/ideacraft/mail/MailController.java
@@ -0,0 +1,47 @@
+package com.ideacraft.mail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MailController {
+
+	private final Logger logger = LogManager.getLogger();
+
+	@GetMapping("/mail/send")
+	public String sendMail() {
+		System.out.println("SEND MAIL TEST");
+		try {
+
+			List<String> recipientList = new ArrayList<>();
+			// from DB
+			recipientList.add("hee_place@naver.com");
+			recipientList.add("wjtmxm7@gmail.com");
+
+			Map<String, String> mailContent = new HashMap<>();
+			// value 수정
+			mailContent.put("mailTitle", "[Test]");
+			mailContent.put("mailHtml", "This is test mail");
+
+			// sender
+
+			// smtp 서버 정보, 메일 내용, 받는 사람리스트
+
+			MailUtil mailUtil = new MailUtil();
+			// 받는사람, 메일내용을 넘김
+			mailUtil.sendMail(recipientList, mailContent);
+
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		return "index";
+	}
+
+}
