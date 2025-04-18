f3e05d1af593b9c369584a695ab5a42c0cdf460c
heeseonYoonn <keepthegreen100@gmail.com>
2025-04-18 10:52:18 +0900

    암호화 소스 중간 커밋

---
+++ src/main/java/com/ideacraft/encrypt/AES256.java
@@ -0,0 +1,56 @@
+package com.ideacraft.encrypt;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class AES256 {
+    // 32바이트(256비트) 키
+    private static final String SECRET_KEY = "12345678901234567890123456789012"; // 32 chars
+    // 16바이트(128비트) IV
+    private static final String IV = "abcdefghijklmnop"; // 16 chars
+
+    // 암호화
+    public static String encrypt(String plainText) throws Exception {
+        /**
+         * Cipher는 암호화 또는 복호화 작업을 수행하기 위한 클래스
+         * 암호화 알고리즘, 블록암호화모드, 패딩방식을 지정해서 사용할 수 있음
+         * -------------------------------------------
+         * 암호화 알고리즘: 블록화된 128비트의 평문을 암호화하는 방식
+         * 블록암호화모드: 이 평문을 어떻게 블록들로 나누고 이걸 어떻게 처리할지, 어떤 순서로 처리할지 등
+         * 패딩방식: 마지막 블록에 대해 128비트를 못채우는 경우 128비트에 남은 데이터를 어떤방식으로 채울지에 대한 내용
+         *-------------------------------------------
+         */
+
+        /**
+         * CBC의 경우 이전 블록의 암호문과 XOR 연산 후 암호화됨
+         * 그래서 첫 블록의 경우 이전 블록이 없으므로 IV (initial vector, 16바이트)를 설정해줘야함
+         */
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+        
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    // 복호화
+    public static String decrypt(String encryptedText) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+
+        return new String(decrypted, StandardCharsets.UTF_8);
+    }
+
+}
--- src/main/java/com/ideacraft/mail/MailController.java
+++ src/main/java/com/ideacraft/mail/MailController.java
@@ -7,15 +7,11 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class MailController {
 
 	private final Logger logger = LogManager.getLogger();
 
-	@GetMapping("/mail/send")
 	public String sendMail() {
 		System.out.println("SEND MAIL TEST");
 		try {
